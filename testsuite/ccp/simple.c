a531da77cbb253c62396f8b0ed2e5f44f54b816a
Giuliano Belinassi <gbelinassi@suse.de>
2023-11-27 22:58:35 -0300

    Mark `simple.c` test as passing.
    Signed-off-by: Giuliano Belinassi <gbelinassi@suse.de>

--- testsuite/ccp/simple.c
+++ testsuite/ccp/simple.c
@@ -1,5 +1,4 @@
 /* { dg-options "-DCE_EXTRACT_FUNCTIONS=pu_f -DCE_NO_EXTERNALIZATION" }*/
-/* { dg-xfail }*/
 
 /* The pu_f symbol location should be printed right before the symbol definition */
 
@@ -7,4 +6,4 @@ void pu_f(void)
 {
 }
 
-/* { dg-final { scan-tree-dump "clang-extract: from testsuite/ccp/simple.c" } } */
+/* { dg-final { scan-tree-dump "clang-extract: from simple.c" } } */
