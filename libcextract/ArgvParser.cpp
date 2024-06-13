2b0f58d5ab7688f86ab28423db0ea2b58c5724b2
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-06-13 13:14:52 -0300

    ArgvParser: Disable incompatible-pointer-types-discard-qualifiers
    This can be happen on older codestreams, so don't bother erroring out in
    those cases.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- libcextract/ArgvParser.cpp
+++ libcextract/ArgvParser.cpp
@@ -88,6 +88,7 @@ void ArgvParser::Insert_Required_Parameters(void)
     // For some reason libtooling do not pass the clang include folder.  Pass this then.
     "-I/usr/lib64/clang/" STRINGFY_VALUE(CLANG_VERSION_MAJOR) "/include",
     "-Wno-gnu-variable-sized-type-not-at-end",
+    "-Wno-incompatible-pointer-types-discards-qualifiers", // this can be triggered for older codestreams
     "-Wno-missing-prototypes", // We remove the static keyword from the
                                // extracted function, so disable warnings
                                // related to it.
