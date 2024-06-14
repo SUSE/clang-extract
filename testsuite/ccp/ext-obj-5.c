f4a390a7bec85e59161cbfaf9576bdfd6fe51c19
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-06-14 13:23:23 -0300

    SymbolExternalizer: Rename externalized operand of sizeof
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

---
+++ testsuite/ccp/ext-obj-5.c
@@ -0,0 +1,17 @@
+/* { dg-options "-DCE_EXTRACT_FUNCTIONS=pu_f -DCE_EXPORT_SYMBOLS=ee_o" }*/
+
+#include <assert.h>
+
+static char ee_o[] = "123";
+
+void pu_f(void)
+{
+        char s[sizeof(ee_o)];
+	char t[sizeof(ee_o)][sizeof(ee_o)];
+	static_assert(sizeof(s) == 4);
+	static_assert(sizeof(t) == 16);
+        ee_o;
+}
+
+/* { dg-final { scan-tree-dump "static char \(\*klpe_ee_o\)\[4\];" } } */
+/* { dg-final { scan-tree-dump "char s\[sizeof\(\(\*klpe_ee_o\)\)\];" } } */
