a290044e46400e7106a73901e4765951f71e3381
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-07-04 10:48:45 -0300

    testsuite: attr: Use esp for current stack point on x86 32bit
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- testsuite/small/attr-1.c
+++ testsuite/small/attr-1.c
@@ -2,6 +2,8 @@
 /* { dg-skip-on-archs "s390x" }*/
 #ifdef __x86_64__
 register unsigned long current_stack_pointer asm("rsp");
+#elif __i386__
+register unsigned long current_stack_pointer asm("esp");
 #elif __aarch64__
 register unsigned long current_stack_pointer asm("sp");
 #elif __PPC64__
