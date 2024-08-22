88f7d523007ce8cb78da2c9234e8fa48f5cdcbfe
Giuliano Belinassi <gbelinassi@suse.de>
2024-08-22 11:29:10 -0300

    Fix symbol visibility
    In the case the symbol is only available on SYMTAB we can't weakly
    externalize it, as the linker will resolve it as not found.
    
    Signed-off-by: Giuliano Belinassi <gbelinassi@suse.de>

--- testsuite/inline/inlined-into-1.c
+++ testsuite/inline/inlined-into-1.c
@@ -1,4 +1,4 @@
-/* { dg-compile "-fdump-ipa-clones -O3 -g3"} */
+/* { dg-compile "-fdump-ipa-clones -O3 -g3 -shared"} */
 /* { dg-options "-where-is-inlined g"} */
 
 static inline int g(void)
@@ -6,17 +6,24 @@ static inline int g(void)
   return 42;
 }
 
-int f()
+static __attribute__((noinline)) h(void)
 {
   return g();
 }
 
+int f()
+{
+  return h() + g();
+}
+
 int main(void)
 {
-  return f();
+  return f() + g();
 }
 
 
 /* { dg-final { scan-tree-dump "f" } } */
 /* { dg-final { scan-tree-dump "g" } } */
-/* { dg-final { scan-tree-dump "FUNC\tPublic" } } */
+/* { dg-final { scan-tree-dump "f *.*FUNC\tPublic symbol\n" } } */
+/* { dg-final { scan-tree-dump "h *.*FUNC\tPrivate symbol\n" } } */
+/* { dg-final { scan-tree-dump "main *.*FUNC\tPublic symbol\n" } } */
